2f7b04019c661ab02cf08343412a0add9d91e9ce
Justinfungi <fhj1371201288@gmail.com>
2023-01-07 18:00:43 +0800

    Python database

--- Database.sh
+++ Database.sh
@@ -1,6 +1,31 @@
 sudo mysql
+sudo mysql -u fish -p
+
+# user
+117
 
 
+To list users:
+
+select user,host from mysql.user;
+To show privileges:
+
+show grants for 'user'@'host';
+To change privileges, first revoke. Such as:
+
+revoke all privileges on *.* from 'user'@'host';
+Then grant the appropriate privileges as desired:
+
+grant SELECT,INSERT,UPDATE,DELETE ON `db`.* TO 'user'@'host';
+Finally, flush:
+
+flush privileges;
+The MySQL documentation is excellent:
+
+
+
+
+# create
 create database Justris
 show databases
 use Justris
